370d8389e2f3084b4b0fdd8fca970c97ccedaf70
Christian Ferreira <christian.ferreira@mohawkcollege.ca>
2017-05-04 17:00:11 -0400

    Started work on Login view

--- FilmHaus/FilmHaus/Startup.cs
+++ FilmHaus/FilmHaus/Startup.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.Owin;
+﻿using FilmHaus.Models.Base;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(FilmHaus.Startup))]
+
 namespace FilmHaus
 {
     public partial class Startup
@@ -9,6 +13,36 @@ namespace FilmHaus
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesAndUsers();
+        }
+
+        private void CreateRolesAndUsers()
+        {
+            FilmHausDbContext context = new FilmHausDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<User>(new UserStore<User>(context));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("Moderator"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Moderator";
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("User"))
+            {
+                var role = new IdentityRole();
+                role.Name = "User";
+                roleManager.Create(role);
+            }
         }
     }
 }
