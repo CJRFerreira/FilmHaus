600a19320d2248c7ef9609956d37660269eae907
Christian Ferreira <christian.ferreira@mohawkcollege.ca>
2017-05-19 09:08:28 -0400

    - Finished outstanding Base & Connector models

--- FilmHaus/FilmHaus/Models/Base/FilmRole.cs
+++ FilmHaus/FilmHaus/Models/Base/FilmRole.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace FilmHaus.Models.Base
 {
+    [Table("FilmRole")]
     public class FilmRole
     {
+        [Key]
+        public Guid Id { get; set; }
+
+        public String Name { get; set; }
     }
 }
--- FilmHaus/FilmHaus/Models/Connector/FilmPersonRole.cs
+++ FilmHaus/FilmHaus/Models/Connector/FilmPersonRole.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using FilmHaus.Models.Base;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,14 +13,14 @@ namespace FilmHaus.Models.Connector
     {
         [Key]
         [Column(Order = 1)]
-        [ForeignKey("Film")]
+        [ForeignKey("FilmPerson")]
         public Guid FilmPersonId { get; set; }
 
         public virtual FilmPerson FilmPerson { get; set; }
 
         [Key]
         [Column(Order = 2)]
-        [ForeignKey("Genre")]
+        [ForeignKey("FilmRole")]
         public Guid FilmRoleId { get; set; }
 
         public virtual FilmRole FilmRole { get; set; }
