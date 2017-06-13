c50649561e24e6c88a9b88cd906dc80d50ae0266
Christian Ferreira <christian.ferreira@mohawkcollege.ca>
2017-06-13 08:54:58 -0400

    - Set-up localization project - Modularized registration and login

---
+++ FilmHaus/FilmHaus/Filters/LanguageActionFilter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilmHaus.Filters
+{
+    public class LanguageActionFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var culture = filterContext.HttpContext.Request.Cookies["_culture"]?.Value;
+
+            if (culture != null)
+            {
+                Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(culture);
+            }
+            else
+            {
+                Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en");
+            }
+        }
+    }
+}
