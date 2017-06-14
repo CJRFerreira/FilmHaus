98ffc974d4e666af46c5026821184a458d1c3b42
Christian Ferreira <christian.ferreira@mohawkcollege.ca>
2017-06-14 09:26:33 -0400

    Updated project libraries

--- FilmHaus/FilmHaus/Scripts/_references.js
+++ FilmHaus/FilmHaus/Scripts/_references.js
@@ -5,4 +5,6 @@
 /// <reference path="jquery-1.9.1.js" />
 /// <reference path="jquery-1.9.1.min.js" />
 /// <reference path="modernizr-2.6.2.js" />
-/// <reference path="respond.js" />
+/// <reference path="modernizr-2.8.3.js" />
+/// <reference path="respond.matchmedia.addlistener.min.js" />
+/// <reference path="respond.min.js" />
