58c63846402de4bd4293fe0fd72b95c683f17776
Christian Ferreira <christian.ferreira@mohawkcollege.ca>
2017-11-03 13:19:03 -0400

    - Added code for default users

--- FilmHaus/FilmHaus/Startup.cs
+++ FilmHaus/FilmHaus/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
 using Owin;
+using System;
 
 [assembly: OwinStartupAttribute(typeof(FilmHaus.Startup))]
 
@@ -14,7 +15,7 @@ namespace FilmHaus
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-            //CreateRolesAndUsers();
+            CreateRolesAndUsers();
         }
 
         private void CreateRolesAndUsers()
@@ -29,6 +30,22 @@ namespace FilmHaus
                 var role = new IdentityRole();
                 role.Name = "Admin";
                 roleManager.Create(role);
+
+                var admin = new User
+                {
+                    FirstName = "Christian",
+                    LastName = "Ferreira",
+                    Email = "christian.ferreira@mohawkcollege.ca",
+                    EmailConfirmed = true,
+                    CreatedOn = DateTime.Now
+                };
+                string adminPassword = "Mohawk_123";
+
+                var userCheck = userManager.Create(admin, adminPassword);
+                if (userCheck.Succeeded)
+                {
+                    var result = userManager.AddToRole(admin.Id, "Admin");
+                }
             }
 
             if (!roleManager.RoleExists("Moderator"))
@@ -36,6 +53,22 @@ namespace FilmHaus
                 var role = new IdentityRole();
                 role.Name = "Moderator";
                 roleManager.Create(role);
+
+                var mod = new User
+                {
+                    FirstName = "Bob",
+                    LastName = "Burger",
+                    Email = "bob.burger@bobsburgers.com",
+                    EmailConfirmed = true,
+                    CreatedOn = DateTime.Now
+                };
+                string modPassword = "Mohawk_123";
+
+                var userCheck = userManager.Create(mod, modPassword);
+                if (userCheck.Succeeded)
+                {
+                    userManager.AddToRole(mod.Id, "Moderator");
+                }
             }
 
             if (!roleManager.RoleExists("User"))
@@ -43,6 +76,22 @@ namespace FilmHaus
                 var role = new IdentityRole();
                 role.Name = "User";
                 roleManager.Create(role);
+
+                var user = new User
+                {
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Email = "john.doe@realemail.co.uk",
+                    EmailConfirmed = true,
+                    CreatedOn = DateTime.Now
+                };
+                string userPassword = "Mohawk_123";
+
+                var userCheck = userManager.Create(user, userPassword);
+                if (userCheck.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "User");
+                }
             }
         }
     }
