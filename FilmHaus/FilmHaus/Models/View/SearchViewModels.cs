5b8a5ace4156261309653e23ec738f5e57cbc2b0
Christian Ferreira <christian.ferreira@mohawkcollege.ca>
2017-11-28 17:08:47 -0500

    - Search models are now cleaner

--- FilmHaus/FilmHaus/Models/View/SearchViewModels.cs
+++ FilmHaus/FilmHaus/Models/View/SearchViewModels.cs
@@ -14,6 +14,24 @@ namespace FilmHaus.Models.View
         {
         }
 
+        public SearchViewModel(string searchTerm, AwardStatus? accolades, int? releaseYear, int? rating)
+        {
+            SearchTerm = searchTerm;
+            Accolades = accolades;
+            ReleaseYear = releaseYear;
+            Rating = rating;
+        }
+
+        public SearchViewModel(SearchViewModel searchViewModel)
+        {
+            SearchTerm = searchViewModel.SearchTerm;
+            Accolades = searchViewModel.Accolades;
+            ReleaseYear = searchViewModel.ReleaseYear;
+            Rating = searchViewModel.Rating;
+        }
+
+        [Required]
+        [Display(Name = "Search", ResourceType = typeof(Locale))]
         public string SearchTerm { get; set; }
 
         [Display(Name = "Accolades", ResourceType = typeof(Locale))]
@@ -26,80 +44,75 @@ namespace FilmHaus.Models.View
         public int? Rating { get; set; }
     }
 
-    public class SearchAllViewModel
+    public class SearchAllViewModel : SearchViewModel
     {
-        public SearchAllViewModel()
+        public SearchAllViewModel() : base()
         {
         }
 
-        public SearchAllViewModel(List<GeneralFilmViewModel> films, List<GeneralShowViewModel> shows)
+        public SearchAllViewModel(string searchTerm, AwardStatus? accolades, int? releaseYear, int? rating, List<GeneralFilmViewModel> films, List<GeneralShowViewModel> shows) : base(searchTerm, accolades, releaseYear, rating)
         {
             Films = films;
             Shows = shows;
         }
 
-        public string SearchTerm { get; set; }
-
-        [Display(Name = "Accolades", ResourceType = typeof(Locale))]
-        public AwardStatus? Accolades { get; set; }
-
-        [Display(Name = "ReleaseDate", ResourceType = typeof(Locale))]
-        public int? ReleaseYear { get; set; }
-
-        [Display(Name = "Rating", ResourceType = typeof(Locale))]
-        public int? Rating { get; set; }
+        public SearchAllViewModel(SearchAllViewModel searchAllViewModel)
+        {
+            SearchTerm = searchAllViewModel.SearchTerm;
+            Accolades = searchAllViewModel.Accolades;
+            ReleaseYear = searchAllViewModel.ReleaseYear;
+            Rating = searchAllViewModel.Rating;
+            Films = searchAllViewModel.Films;
+            Shows = searchAllViewModel.Shows;
+        }
 
         public List<GeneralFilmViewModel> Films { get; set; }
 
         public List<GeneralShowViewModel> Shows { get; set; }
     }
 
-    public class SearchShowsViewModel
+    public class SearchShowsViewModel : SearchViewModel
     {
-        public SearchShowsViewModel()
+        public SearchShowsViewModel() : base()
         {
         }
 
-        public SearchShowsViewModel(List<GeneralShowViewModel> shows)
+        public SearchShowsViewModel(string searchTerm, AwardStatus? accolades, int? releaseYear, int? rating, List<GeneralShowViewModel> shows) : base(searchTerm, accolades, releaseYear, rating)
         {
             Shows = shows;
         }
 
-        public string SearchTerm { get; set; }
-
-        [Display(Name = "Accolades", ResourceType = typeof(Locale))]
-        public AwardStatus? Accolades { get; set; }
-
-        [Display(Name = "ReleaseDate", ResourceType = typeof(Locale))]
-        public int? ReleaseYear { get; set; }
-
-        [Display(Name = "Rating", ResourceType = typeof(Locale))]
-        public int? Rating { get; set; }
+        public SearchShowsViewModel(SearchShowsViewModel searchShowsViewModel)
+        {
+            SearchTerm = searchShowsViewModel.SearchTerm;
+            Accolades = searchShowsViewModel.Accolades;
+            ReleaseYear = searchShowsViewModel.ReleaseYear;
+            Rating = searchShowsViewModel.Rating;
+            Shows = searchShowsViewModel.Shows;
+        }
 
         public List<GeneralShowViewModel> Shows { get; set; }
     }
 
-    public class SearchFilmsViewModel
+    public class SearchFilmsViewModel : SearchViewModel
     {
-        public SearchFilmsViewModel()
+        public SearchFilmsViewModel() : base()
         {
         }
 
-        public SearchFilmsViewModel(List<GeneralFilmViewModel> films)
+        public SearchFilmsViewModel(string searchTerm, AwardStatus? accolades, int? releaseYear, int? rating, List<GeneralFilmViewModel> films) : base(searchTerm, accolades, releaseYear, rating)
         {
             Films = films;
         }
 
-        public string SearchTerm { get; set; }
-
-        [Display(Name = "Accolades", ResourceType = typeof(Locale))]
-        public AwardStatus? Accolades { get; set; }
-
-        [Display(Name = "ReleaseDate", ResourceType = typeof(Locale))]
-        public int? ReleaseYear { get; set; }
-
-        [Display(Name = "Rating", ResourceType = typeof(Locale))]
-        public int? Rating { get; set; }
+        public SearchFilmsViewModel(SearchFilmsViewModel searchFilmsViewModel)
+        {
+            SearchTerm = searchFilmsViewModel.SearchTerm;
+            Accolades = searchFilmsViewModel.Accolades;
+            ReleaseYear = searchFilmsViewModel.ReleaseYear;
+            Rating = searchFilmsViewModel.Rating;
+            Films = searchFilmsViewModel.Films;
+        }
 
         public List<GeneralFilmViewModel> Films { get; set; }
     }
